366c4da9d32f39d967ab563b0ba99f140cfc433f
choiyoung69 <choiyoung609@gmail.com>
2025-11-03 15:49:21 +0900

    test(LottoTest): 당첨 번호 일치 개수/보너스 번호 포함하는지 체크하는 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -66,4 +66,104 @@ class LottoTest {
                 List.of(1, 2, 3, 4, 5, 50)
         );
     }
+
+    @Test
+    @DisplayName("하나도 일치하지 않으면 0을 반환한다")
+    void 일치하지_않음() {
+        List<Integer> winning = List.of(1, 2, 3, 4, 5, 6);
+        Lotto mine = new Lotto(List.of(7, 8, 9, 10, 11, 12));
+
+        int matchCount = mine.countMatch(winning);
+
+        assertThat(matchCount).isZero();
+    }
+
+    @Test
+    @DisplayName("1개 일치하면 1을 반환한다")
+    void 한개_일치() {
+        List<Integer> winning = List.of(1, 2, 3, 4, 5, 6);
+        Lotto mine = new Lotto(List.of(1, 7, 8, 9, 10, 11));
+
+        int matchCount = mine.countMatch(winning);
+
+        assertThat(matchCount).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("2개 일치하면 2을 반환한다")
+    void 두개_일치() {
+        List<Integer> winning = List.of(1, 2, 3, 4, 5, 6);
+        Lotto mine = new Lotto(List.of(1, 2, 7, 8, 9, 10));
+
+        int matchCount = mine.countMatch(winning);
+
+        assertThat(matchCount).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("3개 일치하면 3을 반환한다")
+    void 세개_일치() {
+        List<Integer> winning = List.of(1, 2, 3, 4, 5, 6);
+        Lotto mine = new Lotto(List.of(1, 2, 3, 7, 8, 9));
+
+        int matchCount = mine.countMatch(winning);
+
+        assertThat(matchCount).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("4개 일치하면 4를 반환한다")
+    void 네개_일치() {
+        List<Integer> winning = List.of(1, 2, 3, 4, 5, 6);
+        Lotto mine = new Lotto(List.of(1, 2, 3, 4, 7, 8));
+
+        int matchCount = mine.countMatch(winning);
+
+        assertThat(matchCount).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("5개 일치하면 5를 반환한다")
+    void 다섯개_일치() {
+        List<Integer> winning = List.of(1, 2, 3, 4, 5, 6);
+        Lotto mine = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+
+        int matchCount = mine.countMatch(winning);
+
+        assertThat(matchCount).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("6개 일치하면 6를 반환한다")
+    void 여섯개_일치() {
+        List<Integer> winning = List.of(1, 2, 3, 4, 5, 6);
+        Lotto mine = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        int matchCount = mine.countMatch(winning);
+
+        assertThat(matchCount).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("보너스 번호를 포함하면 true를 반환한다")
+    void 보너스번호_포함() {
+        int bonusNumber = 1;
+        Lotto mine = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        boolean result = mine.containBonusNumber(bonusNumber);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("보너스 번호를 포함하지 않으면 false를 반환한다")
+    void 보너스번호_미포함() {
+        int bonusNumber = 7;
+        Lotto mine = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        boolean result = mine.containBonusNumber(bonusNumber);
+
+        assertThat(result).isFalse();
+    }
+
 }
