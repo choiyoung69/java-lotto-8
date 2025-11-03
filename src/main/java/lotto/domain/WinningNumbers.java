162c108c96f190ecb1fe24fb062af396f4d14a25
choiyoung69 <choiyoung609@gmail.com>
2025-11-03 15:09:05 +0900

    feat(WinningNUmbers): 당첨 번호 저장하는 객체 생성

---
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -0,0 +1,62 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningNumbers {
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+    private final List<Integer> numbers;
+    private final int bonusNumber;
+
+    private WinningNumbers(List<Integer> numbers, int bonusNumber) {
+        this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static WinningNumbers create(List<Integer> numbers, int bonusNumber) {
+        validate(numbers, bonusNumber);
+        return new WinningNumbers(numbers, bonusNumber);
+    }
+
+    private static void validate(List<Integer> numbers, int bonusNumber) {
+        validateNumbersSize(numbers);
+        validateOutOfRangeNumbers(numbers);
+        validateOutOfRangeBonusNumber(bonusNumber);
+        validateNoDuplicate(numbers, bonusNumber);
+    }
+
+    private static void validateNumbersSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private static void validateOutOfRangeNumbers(List<Integer> numbers) {
+        boolean outOfRange = numbers.stream()
+                .anyMatch(number -> number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX);
+        if (outOfRange) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1~45 사이여야 합니다");
+        }
+    }
+
+    private static void validateOutOfRangeBonusNumber(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이여야 합니다");
+        }
+    }
+
+    private static void validateNoDuplicate(List<Integer> numbers, int bonusNumber) {
+        validateNumberNoDuplicate(numbers);
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호와 로또 번호는 중복될 수 없습니다");
+        }
+    }
+
+    private static void validateNumberNoDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+}
--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -52,7 +52,7 @@ public class WinningNumbersTest {
     @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 당첨_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> WinningNumber.create(List.of(1, 2, 3, 4, 5, 5), 6))
+        assertThatThrownBy(() -> WinningNumbers.create(List.of(1, 2, 3, 4, 5, 5), 6))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -78,7 +78,7 @@ public class WinningNumbersTest {
     @DisplayName("당첨 번호와 보너스 번호가 중복되면 예외가 발생한다.")
     @Test
     void 당첨번호와_보너스번호가에_중복되면_예외가_발생한다() {
-        assertThatThrownBy(() -> WinningNumber.create(List.of(1, 2, 3, 4, 5, 6), 6))
+        assertThatThrownBy(() -> WinningNumbers.create(List.of(1, 2, 3, 4, 5, 6), 6))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
