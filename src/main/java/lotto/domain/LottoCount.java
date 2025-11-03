c281c4109daed4f340fda8d8627ed0d4f68ca5e6
choiyoung69 <choiyoung609@gmail.com>
2025-11-03 10:23:59 +0900

    feat(LottoCount): 로또 금액 구현

---
+++ src/main/java/lotto/domain/LottoCount.java
@@ -0,0 +1,37 @@
+package lotto.domain;
+
+public class LottoCount {
+
+    private static final int LOTTO_PRICE_UNIT = 1000;
+    private final int count;
+
+    private LottoCount(int count) {
+        this.count = count;
+    }
+
+    public static LottoCount create(int amount) {
+        validate(amount);
+        return new LottoCount(amount / LOTTO_PRICE_UNIT);
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    private static void validate(int amount) {
+        validatePositiveAmount(amount);
+        validateLottoPriceUnit(amount);
+    }
+
+    private static void validatePositiveAmount(int amount) {
+        if(amount <= 0){
+            throw new IllegalArgumentException("로또 구입 금액은 0원보다 커야 합니다");
+        }
+    }
+
+    private static void validateLottoPriceUnit(int amount) {
+        if (amount % LOTTO_PRICE_UNIT != 0) {
+            throw new IllegalArgumentException("로또 구입 금액은 1000원 단위여야 합니다.");
+        }
+    }
+}
--- src/test/java/lotto/domain/LottoCountTest.java
+++ src/test/java/lotto/domain/LottoCountTest.java
@@ -11,15 +11,15 @@ public class LottoCountTest {
     @Test
     @DisplayName("로또 금액이 정상 입력되었을 때 정상 처리")
     void 로또금액이_정상입력되면_정상처리된다() {
-        LottoCount lottoCount = LottoCount.from(10000);
+        LottoCount lottoCount = LottoCount.create(10000);
 
-        assertThat(lottoCount.getCount()).isEqualto(10);
+        assertThat(lottoCount.getCount()).isEqualTo(10);
     }
 
     @Test
     @DisplayName("로또 금액이 1000원 단위가 아닐 때 에러 처리")
     void 로또금액이_1000원단위가_아닐때_에러처리된다() {
-        assertThatThrownBy(() -> LottoCount.from(10123))
+        assertThatThrownBy(() -> LottoCount.create(10123))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("1000원 단위");
     }
@@ -27,15 +27,15 @@ public class LottoCountTest {
     @Test
     @DisplayName("로또 금액이 음수면 에러 처리")
     void 로또금액이_음수면_예외처리된다() {
-        assertThatThrownBy(() -> LottoCount.from(-1000))
+        assertThatThrownBy(() -> LottoCount.create(-1000))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("0원보다");
     }
 
     @Test
     @DisplayName("로또 금액이 0원이면 에러 처리")
-    void 로또금액이_음수면_예외처리된다() {
-        assertThatThrownBy(() -> LottoCount.from(0))
+    void 로또금액이_0원이면_예외처리된다() {
+        assertThatThrownBy(() -> LottoCount.create(0))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("0원보다");
     }
