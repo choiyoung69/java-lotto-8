b5837eb77a8389ef5c7a3b3a2a56fe8e30ebca04
choiyoung69 <choiyoung609@gmail.com>
2025-11-03 11:29:59 +0900

    feat(LottoGenerator): 로또 발행 관련 인터페이스 구현

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import lotto.domain.generator.LottoGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
