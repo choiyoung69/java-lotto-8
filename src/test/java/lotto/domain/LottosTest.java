33f65ea8cd7eee31b79c41823023aa5f679d37be
choiyoung69 <choiyoung609@gmail.com>
2025-11-03 11:28:26 +0900

    test(LottosTest): 로또 관리 객체(Lottos) 관련 테스트

---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottosTest {
+
+    @Test
+    @DisplayName("로또 개수만큼 로또가 생성된다")
+    void 로또개수만큼_로또가_생성된다() {
+        LottoCount count = LottoCount.create(2000);
+        LottoGenerator generator = () -> new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        Lottos lottos = Lottos.create(count, generator);
+
+        assertThat(lottos.getLottos()).hasSize(2);
+    }
+
+}
